d2191e0b06c711305013a0322cb41cf60bc9432d
NATHAN <ndickson@student.nsw.edu.au>
2025-03-11 14:18:13 +1100

    got a clear terminal function and connected the earth questions to main code

--- earth.py
+++ earth.py
@@ -1,16 +1,22 @@
 questions = [
-    {"Question":"How was the moon created?","Options":["a) Accretion from elements of a once Nebula star formed into a moon like structure.","b) Dairy factories created a huge sphere of cheese and sent it to space.","c) A planet the size of Mars hit Earth forming a moon strcutre from Earths rubble.","d) A commit from a nearby galaxy got caught in Earth's gravitational pull and became the moon."],"Answer": "c"},
+    {"Question":"How was the moon created?","Options":["a) Accretion from elements of a once Nebula star formed into a moon like structure.","b) Dairy factories created a huge sphere of cheese and sent it to space.","c) A planet the size of Mars hit Earth forming a moon structure from Earths rubble.","d) A comet from a nearby galaxy got caught in Earth's gravitational pull and became the moon."],"Answer": "c"},
+                  
 
 ]
 
-for question in questions:
-    print(question["Question"])
-    print(f"Your options are:")
-    for option in question["Options"]:
-        print(option)
 
-    answer = input("What do you choose? ") #y=
-    if answer == question["Answer"]:
-        print("Well done!!!")
-    else:
-        print("Wrong")
+# os.system('cls' if os.name == 'nt' else 'clear')
+# for question in questions:
+#     print(question["Question"])
+#     print(f"Your options are:")
+#     for option in question["Options"]:
+#         print(option)
+
+#     answer = input("What do you choose? ") #y=
+#     if answer == question["Answer"]:
+#         print("Well done!!!")
+#     else:
+#         print("Wrong")
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from questions import questions
+# interacts with operating system 
+import os
+
+from earth import questions
 
 
-
-
-def print_questions(questions):
-    for question in questions:
+def print_questions(earth):
+    # clear the screen
+    from earth import questions
+    for question in earth:
         print(question["Question"])
 
 def intro():
@@ -14,4 +17,5 @@ def intro():
 def main():
     intro()
 
+os.system('cls' if os.name == 'nt' else 'clear')
 main()
