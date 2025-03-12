3fae1dbd13958f9406881c41f152c7db1d84e528
NATHAN <ndickson@student.nsw.edu.au>
2025-03-12 15:25:42 +1100

    added shuffle and topics

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ def earth_questions(earth):
     for question in earth:
         print(question["Question"])
 
+def filter_topic(questions):
+    topics = []
+    for question in questions:
+        topics.append(question["Topic"])
+    unique_topics = list(set(topics))
+
+    print("The topics are.....:")
+    for topic in unique_topics:
+        print(topic)
+
 def intro():
     print("Hello!")
-    earth_questions(questions)
+    filter_topic(questions)
 
 def main():
     intro()
