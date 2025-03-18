3dd05ce28665a9fd15627b3126ed190835238539
NATHAN <ndickson@student.nsw.edu.au>
2025-03-18 21:45:04 +1100

    Mum helped correct gramma and I did lots of \n

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ def earth_questions(questions):
     os.system('cls' if os.name == 'nt' else 'clear')
     random.shuffle(questions)
 
-    decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
+    decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
     print("\n\n")
     # check if the input is valid
     while decision.lower() != ("short") and decision.lower() != ("medium") and decision.lower() != ("long"):
      # repeat the question with a warning
         typewriter("Invalid choice. Please type a valid length ")
         print("\n\n")
-        decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") 
+        decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") 
         print("\n\n")
  
     if decision.lower() == "short":
@@ -90,49 +90,48 @@ def earth_questions(questions):
 
     percentage = score / question_amount * 100
 
-    typewriter("You got {:.2f}% on this test.".format(percentage))
-    typewriter("Your rank is...")
+    typewriter("You got {:.2f}% on this test.\n".format(percentage))
+    typewriter("Your rank is... ")
     time.sleep(3)
     if percentage <= 20:
-        typewriter("Talc!")
+        typewriter("Talc!\n")
         time.sleep(1.5)
-        typewriter("You crumble under the slightest pressure. A mere touch can wear you down, ")
-        typewriter("and even a fingernail is too much for you. You’re soft, flaky, and honestly,")
-        typewriter("no one’s picking you first for the team. But hey, at least you're useful in baby powder. ")
+        typewriter("You crumble under the slightest pressure. A mere touch can wear you down, \n")
+        typewriter("and even a fingernail is too much for you. You’re soft, flaky, and honestly,\n")
+        typewriter("no one’s picking you first for the team. But hey, at least you're useful in baby powder. \n")
         
     elif percentage <= 40:
-        typewriter("Gypsum!")
+        typewriter("Gypsum!\n")
         time.sleep(1.5)
-        typewriter("You try, but you're still easily scratched, a simple copper coin can still leave a mark on you.")
-        typewriter("At least you're useful in drywall and plaster, holding houses together.")
-        typewriter("Just don’t expect to be the foundation.")
+        typewriter("You try, but you're still easily scratched, a simple copper coin can still leave a mark on you.\n")
+        typewriter("At least you're useful in drywall and plaster, holding houses together.\n")
+        typewriter("Just don’t expect to be the foundation.\n")
         
 
     elif percentage <= 60:
-        typewriter("Apatite!")
+        typewriter("Apatite!\n")
         time.sleep(1.5)
-        typewriter("You’re no pushover, but you’re not indestructible either. Harder than your weaker cousins, ")
-        typewriter("you’re tough enough to resist everyday wear but still won’t last against the real heavyweights.") 
-        typewriter("You help build bones and teeth, so at least you're part of the reason people can actually chew their food.")
+        typewriter("You’re no pushover, but you’re not indestructible either. Harder than your weaker cousins, \n")
+        typewriter("you’re tough enough to resist everyday wear but still won’t last against the real heavyweights.\n") 
+        typewriter("You help build bones and teeth, so at least you're part of the reason people can actually chew their food.\n")
 
 
     elif percentage <= 80:
-        typewriter("Quartz!")
+        typewriter("Quartz!\n")
         time.sleep(1.5)
-        typewriter("You’re tough, durable, and versatile. You make up sand, glass, and even tech components.")
-        typewriter("You might not be the hardest, but people rely on you for watches, buildings, and even some beautiful gemstones.")
-        typewriter("You’ve earned your place.")
+        typewriter("You’re tough, durable, and versatile. You make up sand, glass, and even tech components.\n")
+        typewriter("You might not be the hardest, but people rely on you for watches, buildings, and even some beautiful gemstones.\n")
+        typewriter("You’ve earned your place.\n")
 
 
     else:
-        typewriter("Diamond!")
+        typewriter("Diamond!\n")
         time.sleep(1.5)
-        typewriter("You’ve been under immense pressure, and instead of breaking, ")
-        typewriter("you became the toughest mineral on Earth.")
-        typewriter("No one scratches you, and you shine bright, whether in industry or in luxury. ")
-        typewriter("You’re the ultimate performer, proving that pressure makes diamonds!")
+        typewriter("You’ve been under immense pressure, and instead of breaking, \n")
+        typewriter("you became the toughest mineral on Earth.\n")
+        typewriter("No one scratches you, and you shine bright, whether in industry or in luxury. \n")
+        typewriter("You’re the ultimate performer, proving that pressure makes diamonds!\n")
 
-    ###
 
     if wrong_answers:
         typewriter("\nYou got the following questions wrong:")
@@ -165,7 +164,7 @@ def earth_questions(questions):
         
 
         if decision.lower() == "quiz":
-            decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+            decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
             print("\n\n")
             while decision.lower() != "earth" and decision.lower() != "chemistry" and decision.lower() != "biology":
                 typewriter("Invalid choice. Please type a valid topic ")
@@ -194,14 +193,14 @@ def chemistry_questions(questions):
     os.system('cls' if os.name == 'nt' else 'clear')
     random.shuffle(questions)
 
-    decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
+    decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
     typewriter("\n\n")
     # check if the input is valid
     while decision.lower() != ("short") and decision.lower() != ("medium") and decision.lower() != ("long"):
      # repeat the question with a warning
         typewriter("Invalid choice. Please type a valid length ")
         print("\n\n")
-        decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") 
+        decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") 
         print("\n\n")
  
     if decision.lower() == "short":
@@ -252,44 +251,44 @@ def chemistry_questions(questions):
 
     percentage = score / question_amount * 100
 
-    typewriter("You got {:.2f}% on this test.".format(percentage))
-    typewriter("Your rank is...")
+    typewriter("You got {:.2f}% on this test.\n".format(percentage))
+    typewriter("Your rank is... ")
     time.sleep(3)
     if percentage <= 20:
-        typewriter("Francium!")
+        typewriter("Francium!\n")
         time.sleep(1.5)
-        typewriter("You barely exist before breaking down! The most unstable of the alkali metals,")
-        typewriter("you’re radioactive, extremely rare, and decay in seconds.")
-        typewriter("No one can even study you properly before you disappear.")
+        typewriter("You barely exist before breaking down! The most unstable of the alkali metals,\n")
+        typewriter("you’re radioactive, extremely rare, and decay in seconds.\n")
+        typewriter("No one can even study you properly before you disappear.\n")
 
 
     elif percentage <= 40:
-        typewriter("Lithium!")
+        typewriter("Lithium!\n")
         time.sleep(1.5)
-        typewriter("You’re soft, reactive, and float on water.")
-        typewriter("You might not be the toughest, but you're crucial in batteries, keeping the world’s tech running.")
+        typewriter("You’re soft, reactive, and float on water.\n")
+        typewriter("You might not be the toughest, but you're crucial in batteries, keeping the world’s tech running.\n")
 
     elif percentage <= 60:
-        typewriter("Iron!")
+        typewriter("Iron!\n")
         time.sleep(1.5)
-        typewriter("You’re strong, magnetic, and essential for life without you, blood wouldn’t carry oxygen.")
-        typewriter("You rust under pressure, but no one can deny you’re a foundation of civilization!")
+        typewriter("You’re strong, magnetic, and essential for life without you, blood wouldn’t carry oxygen.\n")
+        typewriter("You rust under pressure, but no one can deny you’re a foundation of civilization!\n")
         
 
     elif percentage <= 80:
-        typewriter("Carbon!")
+        typewriter("Carbon!\n")
         time.sleep(1.5)
-        typewriter("You can be soft as graphite or harder than steel as graphene.")
-        typewriter("Shape-shifter, tech innovator, life giver—carbon does it all.")
-        typewriter("You hold life together and make up the hardest known natural material.")
+        typewriter("You can be soft as graphite or harder than steel as graphene.\n")
+        typewriter("Shape-shifter, tech innovator, life giver—carbon does it all.\n")
+        typewriter("You hold life together and make up the hardest known natural material.\n")
 
 
     else:
-        typewriter("Uranium!")
+        typewriter("Uranium!\n")
         time.sleep(1.5)
-        typewriter("You’re heavy, unstable, and unstoppable. You pulse with raw nuclear energy, capable of powering cities or unleashing unimaginable destruction.")
-        typewriter("Your radioactive glow is a warning and a promise wherever you go, ")
-        typewriter("change follows. Whether fueling the future or shaping history, you are a force that cannot be ignored.")
+        typewriter("You’re heavy, unstable, and unstoppable. You pulse with raw nuclear energy, capable of powering cities or unleashing unimaginable destruction.\n")
+        typewriter("Your radioactive glow is a warning and a promise wherever you go, \n")
+        typewriter("change follows. Whether fueling the future or shaping history, you are a force that cannot be ignored.\n")
     
     ###
 
@@ -324,12 +323,12 @@ def chemistry_questions(questions):
         
 
         if decision.lower() == "quiz":
-            decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+            decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
             print("\n\n")
             while decision.lower() != "earth" and decision.lower() != "chemistry" and decision.lower() != "biology":
                 typewriter("Invalid choice. Please type a valid topic ")
                 print("\n\n")
-                decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+                decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
             
             if decision.lower() == "earth":
                 earth_questions(questions)
@@ -353,14 +352,14 @@ def biology_questions(questions):
     os.system('cls' if os.name == 'nt' else 'clear')
     random.shuffle(questions)
 
-    decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
+    decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
     typewriter("\n\n")
     # check if the input is valid
     while decision.lower() != ("short") and decision.lower() != ("medium") and decision.lower() != ("long"):
      # repeat the question with a warning
         typewriter("Invalid choice. Please type a valid length ")
         typewriter("\n\n")
-        decision = input("What lenth of quiz are you wanting to do? Type: | short | medium | long | ") 
+        decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") 
         typewriter("\n\n")
  
     if decision.lower() == "short":
@@ -411,44 +410,44 @@ def biology_questions(questions):
 
     percentage = score / question_amount * 100
 
-    typewriter("You got {:.2f}% on this test.".format(percentage))
-    typewriter("Your rank is...")
+    typewriter("You got {:.2f}% on this test.\n".format(percentage))
+    typewriter("Your rank is... ")
     time.sleep(3)
     if percentage <= 20:
-        typewriter("Mycoplasma!")
+        typewriter("Mycoplasma!\n")
         time.sleep(1.5)
-        typewriter("You’re microscopic, fragile, and barely holding it together.")
-        typewriter("With no cell wall, even antibiotics push you around.")
-        typewriter("You survive by leeching off stronger organisms, parasitic and pathetic.")
+        typewriter("You’re microscopic, fragile, and barely holding it together.\n")
+        typewriter("With no cell wall, even antibiotics push you around.\n")
+        typewriter("You survive by leeching off stronger organisms, parasitic and pathetic.\n")
         
     elif percentage <= 40:
-        typewriter("Amoeba!")
+        typewriter("Amoeba!\n")
         time.sleep(1.5)
-        typewriter("You’ve got no backbone, no real structure, and you move by oozing around.")
-        typewriter("But don’t underestimate yourself your adaptability lets you survive in places others can’t.")
+        typewriter("You’ve got no backbone, no real structure, and you move by oozing around.\n")
+        typewriter("But don’t underestimate yourself your adaptability lets you survive in places others can’t.\n")
         
 
     elif percentage <= 60:
-        typewriter("Mushroom!")
+        typewriter("Mushroom!\n")
         time.sleep(1.5)
-        typewriter("You work in the shadows, breaking down the dead and recycling life itself.")
-        typewriter("Your roots spread like an underground network, influencing entire ecosystems.")
-        typewriter("People might not always notice you, but without you, nature falls apart.")
+        typewriter("You work in the shadows, breaking down the dead and recycling life itself.\n")
+        typewriter("Your roots spread like an underground network, influencing entire ecosystems.\n")
+        typewriter("People might not always notice you, but without you, nature falls apart.\n")
 
 
     elif percentage <= 80:
-        typewriter("Redwood Tree!")
+        typewriter("Redwood Tree!\n")
         time.sleep(1.5)
-        typewriter("You don’t need to move when you’re this big.")
-        typewriter("Towering over the competition, you’ve stood strong for centuries, unshaken by storms, fires, or time.")
-        typewriter("You breathe life into the world, and even when you fall, you shape ecosystems for generations.")
+        typewriter("You don’t need to move when you’re this big.\n")
+        typewriter("Towering over the competition, you’ve stood strong for centuries, unshaken by storms, fires, or time.\n")
+        typewriter("You breathe life into the world, and even when you fall, you shape ecosystems for generations.\n")
 
     else:
-        typewriter("Orca!")
+        typewriter("Orca!\n")
         time.sleep(1.5)
-        typewriter("You dominate the ocean with intelligence, teamwork, and raw power.")
-        typewriter("No natural predators, no real threats you decide who lives and who dies.")
-        typewriter("Whether hunting in packs or outsmarting your prey, you rule your domain like no other.")
+        typewriter("You dominate the ocean with intelligence, teamwork, and raw power.\n")
+        typewriter("No natural predators, no real threats you decide who lives and who dies.\n")
+        typewriter("Whether hunting in packs or outsmarting your prey, you rule your domain like no other.\n")
         typewriter("You are the king of the sea.")
     
     ###
@@ -484,12 +483,12 @@ def biology_questions(questions):
         
 
         if decision.lower() == "quiz":
-            decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+            decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
             typewriter("\n\n")
             while decision.lower() != "earth" and decision.lower() != "chemistry" and decision.lower() != "biology":
                 typewriter("Invalid choice. Please type a valid topic ")
                 typewriter("\n\n")
-                decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+                decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
             
             if decision.lower() == "earth":
                 earth_questions(questions)
@@ -512,6 +511,7 @@ def filter_topic(questions):
     typewriter("The following science quizzes are available: \n\n")
     for topic in unique_topics:
         print(topic)
+        print("\n")
 
     decision = input("What science quiz are you wanting to try? ") # first of all get the input
     typewriter("\n\n")
