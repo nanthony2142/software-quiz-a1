63820082f0417a957685719bc5baf47e2c34dbb1
NATHAN <ndickson@student.nsw.edu.au>
2025-03-21 20:42:20 +1100

    fixed rank system

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from earth import questions
 from earth import ranks
 
 
-
 def typewriter(words): # allows type writting effect
     for char in words:
         time.sleep(0.05) # time for letters to apear
@@ -27,7 +26,7 @@ def quiz_questions(filtered_questions, score, question_number, question_amount, 
     os.system('cls' if os.name == 'nt' else 'clear')
     random.shuffle(filtered_questions)
 
-    # print(ranks["Rank"])
+    
 
     decision = input("What length of quiz are you wanting to do? Type: | short | medium | long | ") # first of all get the input
     print("\n\n")
@@ -85,105 +84,113 @@ def quiz_questions(filtered_questions, score, question_number, question_amount, 
             os.system('cls' if os.name == 'nt' else 'clear')
             number_of_questions = number_of_questions + 1
 
+    return score, question_number, question_amount, number_of_questions, wrong_answers
 
 
-def quiz_results(filtered_ranks, score, question_amount, wrong_answers):
+
+def quiz_results(filtered_ranks, score, question_amount, wrong_answers, percentage):
     os.system('cls' if os.name == 'nt' else 'clear')
     percentage = score / question_amount * 100
 
     typewriter("You got {:.2f}% on this test.\n".format(percentage))
-    typewriter("Your rank is... ")
-    time.sleep(3)
-    if percentage <= 20:
-        typewriter("Talc!\n")
-        time.sleep(1.5)
-        typewriter("You crumble under the slightest pressure. A mere touch can wear you down, \n")
-        typewriter("and even a fingernail is too much for you. You’re soft, flaky, and honestly,\n")
-        typewriter("no one’s picking you first for the team. But hey, at least you're useful in baby powder. \n")
-        
-    elif percentage <= 40:
-        typewriter("Gypsum!\n")
-        time.sleep(1.5)
-        typewriter("You try, but you're still easily scratched, a simple copper coin can still leave a mark on you.\n")
-        typewriter("At least you're useful in drywall and plaster, holding houses together.\n")
-        typewriter("Just don’t expect to be the foundation.\n")
+    typewriter("Your rank is... \n")
+    time.sleep(1.5)
+
+    for rank in filtered_ranks:
+        if percentage <= rank["Test_Percentage"]:
+                typewriter("{}\n".format(rank["Rank"]))
+                typewriter(rank["Description"])
+                time.sleep(2)
+                break
+
+    # if percentage <= 20:
+    #     print[(filtered_ranks[0]["Rank"])]
+    #     time.sleep(1.5)
         
 
-    elif percentage <= 60:
-        typewriter("Apatite!\n")
-        time.sleep(1.5)
-        typewriter("You’re no pushover, but you’re not indestructible either. Harder than your weaker cousins, \n")
-        typewriter("you’re tough enough to resist everyday wear but still won’t last against the real heavyweights.\n") 
-        typewriter("You help build bones and teeth, so at least you're part of the reason people can actually chew their food.\n")
+    # elif percentage <= 40:
+    #     typewriter("Gypsum!\n")
+    #     time.sleep(1.5)
+    #     typewriter("You try, but you're still easily scratched, a simple copper coin can still leave a mark on you.\n")
+    #     typewriter("At least you're useful in drywall and plaster, holding houses together.\n")
+    #     typewriter("Just don’t expect to be the foundation.\n")
+        
+
+    # elif percentage <= 60:
+    #     typewriter("Apatite!\n")
+    #     time.sleep(1.5)
+    #     typewriter("You’re no pushover, but you’re not indestructible either. Harder than your weaker cousins, \n")
+    #     typewriter("you’re tough enough to resist everyday wear but still won’t last against the real heavyweights.\n") 
+    #     typewriter("You help build bones and teeth, so at least you're part of the reason people can actually chew their food.\n")
 
 
-    elif percentage <= 80:
-        typewriter("Quartz!\n")
-        time.sleep(1.5)
-        typewriter("You’re tough, durable, and versatile. You make up sand, glass, and even tech components.\n")
-        typewriter("You might not be the hardest, but people rely on you for watches, buildings, and even some beautiful gemstones.\n")
-        typewriter("You’ve earned your place.\n")
+    # elif percentage <= 80:
+    #     typewriter("Quartz!\n")
+    #     time.sleep(1.5)
+    #     typewriter("You’re tough, durable, and versatile. You make up sand, glass, and even tech components.\n")
+    #     typewriter("You might not be the hardest, but people rely on you for watches, buildings, and even some beautiful gemstones.\n")
+    #     typewriter("You’ve earned your place.\n")
 
 
-    else:
-        typewriter("Diamond!\n")
-        time.sleep(1.5)
-        typewriter("You’ve been under immense pressure, and instead of breaking, \n")
-        typewriter("you became the toughest mineral on Earth.\n")
-        typewriter("No one scratches you, and you shine bright, whether in industry or in luxury. \n")
-        typewriter("You’re the ultimate performer, proving that pressure makes diamonds!\n")
+    # else:
+    #     typewriter("Diamond!\n")
+    #     time.sleep(1.5)
+    #     typewriter("You’ve been under immense pressure, and instead of breaking, \n")
+    #     typewriter("you became the toughest mineral on Earth.\n")
+    #     typewriter("No one scratches you, and you shine bright, whether in industry or in luxury. \n")
+    #     typewriter("You’re the ultimate performer, proving that pressure makes diamonds!\n")
 
 
-    if wrong_answers:
-        typewriter("\nYou got the following questions wrong:")
-        print("\n")
-        for wrong in wrong_answers:
-            typewriter(wrong["Question"])
-            print("\n")
-            typewriter("Answer: {}\n".format(wrong["Explanation"]))
-    else:
-        typewriter("\nYou got all the questions right!")
+    # if wrong_answers:
+    #     typewriter("\nYou got the following questions wrong:")
+    #     print("\n")
+    #     for wrong in wrong_answers:
+    #         typewriter(wrong["Question"])
+    #         print("\n")
+    #         typewriter("Answer: {}\n".format(wrong["Explanation"]))
+    # else:
+    #     typewriter("\nYou got all the questions right!")
 
-    decision = input("Would you like to try the test again? Type: | yes | no | ")
-    print("\n\n")
-    while decision.lower() != "yes" and decision.lower() != "no":
-        typewriter("Invalid choice. Please type a valid option ")
-        print("\n\n")
-        decision = input("Would you like to try the test again? Type: | yes | no | ")
+    # decision = input("Would you like to try the test again? Type: | yes | no | ")
+    # print("\n\n")
+    # while decision.lower() != "yes" and decision.lower() != "no":
+    #     typewriter("Invalid choice. Please type a valid option ")
+    #     print("\n\n")
+    #     decision = input("Would you like to try the test again? Type: | yes | no | ")
     
-    if decision.lower() == "yes":
-        os.system('cls' if os.name == 'nt' else 'clear')
-        chemistry_questions(questions)
+    # if decision.lower() == "yes":
+    #     os.system('cls' if os.name == 'nt' else 'clear')
+    #     chemistry_questions(questions)
 
-    if decision.lower() == "no":
-        decision = input("Would you like to try a different quiz or exit? Type: | quiz | exit | ")  
-        print("\n\n")
-        while decision.lower() != "quiz" and decision.lower() != "exit":
-            typewriter("Invalid choice. Please type a valid option ")
-            print("\n\n")
-            decision = input("Would you like to try a different quiz or exit? Type: | quiz | exit | ")
+    # if decision.lower() == "no":
+    #     decision = input("Would you like to try a different quiz or exit? Type: | quiz | exit | ")  
+    #     print("\n\n")
+    #     while decision.lower() != "quiz" and decision.lower() != "exit":
+    #         typewriter("Invalid choice. Please type a valid option ")
+    #         print("\n\n")
+    #         decision = input("Would you like to try a different quiz or exit? Type: | quiz | exit | ")
         
 
-        if decision.lower() == "quiz":
-            decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
-            print("\n\n")
-            while decision.lower() != "earth" and decision.lower() != "chemistry" and decision.lower() != "biology":
-                typewriter("Invalid choice. Please type a valid topic ")
-                print("\n\n")
-                decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
+    #     if decision.lower() == "quiz":
+    #         decision = input("What science quiz are you wanting to try? Type: | Earth | Chemistry | Biology | ")
+    #         print("\n\n")
+    #         while decision.lower() != "earth" and decision.lower() != "chemistry" and decision.lower() != "biology":
+    #             typewriter("Invalid choice. Please type a valid topic ")
+    #             print("\n\n")
+    #             decision = input("What science quiz are you wanting to try? Type: | earth | chemistry | biology | ")
             
-            if decision.lower() == "earth":
-                earth_questions(questions)
+    #         if decision.lower() == "earth":
+    #             earth_questions(questions)
 
-            elif decision.lower() == "chemistry":
-                chemistry_questions(questions)
+    #         elif decision.lower() == "chemistry":
+    #             chemistry_questions(questions)
             
-            elif decision.lower() == "biology":
-                biology_questions(questions)
+    #         elif decision.lower() == "biology":
+    #             biology_questions(questions)
 
-        if decision.lower() == "exit":
-            ending()
-    return score, question_number, question_amount, number_of_questions, wrong_answers
+    #     if decision.lower() == "exit":
+    #         ending()
+    return filtered_ranks, score, question_amount, wrong_answers
 
 def display_topics(questions):
     topics = []
@@ -211,13 +218,7 @@ def display_topics(questions):
 def filter_topic(questions, topic_choice):
     
     filtered_questions = [question for question in questions if question["Topic"].lower() == topic_choice.lower()]
-    # filtered_ranks = [question["Rank"] for question in filtered_questions]
-    filtered_ranks = 0
-    # score = 0
-    # question_number = 0
-    # question_amount = 0
-    # number = 0
-    # wrong_answers = []
+    filtered_ranks = [rank for rank in ranks if rank["Topic"].lower() == topic_choice.lower()]
     return filtered_questions, filtered_ranks
         
 
@@ -228,20 +229,20 @@ def intro():
     
 
 def main():
-
     # variables used in quiz
     score = 0
     question_number = 0
     question_amount = 0 
     number_of_questions = 0
     wrong_answers = []
+    percentage = 0
 
     # main functions used in the program
     intro() 
     topic_choice = display_topics(questions)
     filtered_questions, filtered_ranks = filter_topic(questions, topic_choice)
-    quiz_questions(filtered_questions, score, question_number, question_amount, number_of_questions, wrong_answers)
-    quiz_results(filtered_ranks, score, question_amount, wrong_answers)
+    score, question_number, question_amount, number_of_questions, wrong_answers = quiz_questions(filtered_questions, score, question_number, question_amount, number_of_questions, wrong_answers)
+    filtered_ranks, score, question_amount, wrong_answers, percentage = quiz_results(filtered_ranks, score, question_amount, wrong_answers, percentage)
 
 
     
